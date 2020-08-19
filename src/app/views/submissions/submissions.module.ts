5ba76d8763bf62a9956f1b2cb4d3a33ba1f5e38f
Manan Mistry <manan.cba1718@ict.gnu.ac.in>
2020-08-19 19:34:26 +0530

    view-submissions-added

--- src/app/views/submissions/submissions.module.ts
+++ src/app/views/submissions/submissions.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SubmissionsRoutingModule } from './submissions-routing.module';
+import { ViewSubmissionsComponent } from './view-submissions/view-submissions.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ViewSubmissionsComponent],
   imports: [
     CommonModule,
     SubmissionsRoutingModule
