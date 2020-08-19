ce3823bde9687dee529906804bb309464a6138ff
Manan Mistry <manan.cba1718@ict.gnu.ac.in>
2020-08-19 21:02:09 +0530

    view-submission-completed

--- src/app/views/submissions/submissions-routing.module.ts
+++ src/app/views/submissions/submissions-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ViewSubmissionsComponent} from './view-submissions/view-submissions.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: ':id',
+    component: ViewSubmissionsComponent,
+    data: {
+      title: 'View Assignments'
+    }
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
