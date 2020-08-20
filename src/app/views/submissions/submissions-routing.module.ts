21008785a29f133c9a61197d560263a11c8f821e
Manan Mistry <manan.cba1718@ict.gnu.ac.in>
2020-08-20 17:28:01 +0530

    view-submission-answers-completed

--- src/app/views/submissions/submissions-routing.module.ts
+++ src/app/views/submissions/submissions-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ViewSubmissionsComponent} from './view-submissions/view-submissions.component';
+import {ViewSubmissionsAnswersComponent} from './view-submissions-answers/view-submissions-answers.component';
 
 
 const routes: Routes = [
@@ -8,9 +9,15 @@ const routes: Routes = [
     path: ':id',
     component: ViewSubmissionsComponent,
     data: {
-      title: 'View Assignments'
+      title: 'View Submissions'
     }
-
+  },
+  {
+    path: ':id/answers',
+    component: ViewSubmissionsAnswersComponent,
+    data: {
+      title: 'View Submission Answers'
+    }
   }
 ];
 
--- src/app/views/submissions/submissions.module.ts
+++ src/app/views/submissions/submissions.module.ts
@@ -4,13 +4,17 @@ import { CommonModule } from '@angular/common';
 import { SubmissionsRoutingModule } from './submissions-routing.module';
 import { ViewSubmissionsComponent } from './view-submissions/view-submissions.component';
 import { ViewSubmissionsAnswersComponent } from './view-submissions-answers/view-submissions-answers.component';
+import {CodeEditorModule} from '../code-editor/code-editor.module';
+import {CollapseModule} from 'ngx-bootstrap/collapse';
 
 
 @NgModule({
   declarations: [ViewSubmissionsComponent, ViewSubmissionsAnswersComponent],
   imports: [
     CommonModule,
-    SubmissionsRoutingModule
+    SubmissionsRoutingModule,
+    CodeEditorModule,
+    CollapseModule
   ]
 })
 export class SubmissionsModule { }
