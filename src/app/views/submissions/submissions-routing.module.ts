601b2005570817fc16382d261e61b2a0fed9d212
Nikhil Malviya <nikhilmalviya1662000@gmail.com>
2020-08-23 17:39:38 +0530

    DONE: user-assignment-view

--- src/app/views/submissions/submissions-routing.module.ts
+++ src/app/views/submissions/submissions-routing.module.ts
@@ -5,12 +5,20 @@ import {ViewQuestionComponent} from './view-question/view-question.component';
 import {ViewSubmissionsComponent} from './view-submissions/view-submissions.component';
 import {ViewSubmissionsAnswersComponent} from './view-submissions-answers/view-submissions-answers.component';
 import {ViewAnswerComponent} from './view-answer/view-answer.component';
+import {UserAssignmentViewComponent} from './user-assignment-view/user-assignment-view.component';
 
 
 const routes: Routes = [
   {
     path: '',
     component: ViewQuestionComponent,
+    data: {
+      title: 'View Question'
+    }
+  },
+  {
+    path: 'assignments',
+    component: UserAssignmentViewComponent,
     data: {
       title: 'View Question'
     }
