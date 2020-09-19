561fe4288a372011b77cb06d3e07eac8e9f50a3c
Nikhil Malviya <nikhilmalviya1662000@gmail.com>
2020-09-20 00:33:12 +0530

    edit-delete-toggle added for assignments

--- src/app/views/assignments/assignment-details/assignment-details.component.ts
+++ src/app/views/assignments/assignment-details/assignment-details.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AssignmentsService } from '../../../services/assignments.service';
 import { Assignment } from '../../../models/assignment';
+import { DataService } from '../../../services/data.service';
 
 @Component({
   templateUrl: './assignment-details.component.html',
@@ -12,6 +13,8 @@ export class AssignmentDetailsComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private assignmentService: AssignmentsService,  
+    private router: Router,
+    private dataService: DataService
   ) { 
     this.assignment = new Assignment();
   }
@@ -19,5 +22,25 @@ export class AssignmentDetailsComponent implements OnInit {
   async ngOnInit() {
     const slug  = this.route.snapshot.params.slug;
     this.assignment = await this.assignmentService.getAssignmentBySlug(slug).toPromise()
+  }
+  toggleStatus(): void {
+    this.assignmentService.toggleAssignmentStatus(this.assignment.id).subscribe(
+      () => {
+        const current = this.assignment.status;
+        this.assignment.status = current === "ACTIVE" ? "INACTIVE": "ACTIVE";
+      }
+    )
+  }
+  editAssignment(){
+    this.dataService.data = this.assignment;
+    this.router.navigate(['edit'],{relativeTo: this.route})
+  }
+  deleteAssignment(): void {
+    this.assignmentService.deleteAssignment(this.assignment.id).subscribe(
+      () => {
+        this.router.navigate(['assignments']);
+      },
+      console.error
+    )
   }
 }
--- src/app/views/assignments/create-assignment/create-assignment.component.ts
+++ src/app/views/assignments/create-assignment/create-assignment.component.ts
@@ -1,24 +1,31 @@
-import {Component, OnInit, AfterViewInit, ViewChild, ViewChildren, QueryList, ElementRef} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AceEditorComponent} from 'ng2-ace-editor';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {Assignment} from '../../../models/assignment';
 import {AssignmentsService} from '../../../services/assignments.service';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
+import { Location } from '@angular/common';
+import { DataService } from '../../../services/data.service';
 // import * as ace from 'ace-builds';
 // import 'ace-builds/webpack-resolver';
 @Component({
   selector: 'app-create-assignment',
   templateUrl: './create-assignment.component.html',
-  styleUrls: ['./create-assignment.component.css']
+  styleUrls: ['./create-assignment.component.css'],
 })
 export class CreateAssignmentComponent implements OnInit {
   assignmentForm: FormGroup;
-
+  isUpdateMode: boolean = false;
+  assignmentID: string;
   // @ViewChild('markdownEditor') markdownEditor: AceEditorComponent;
   // @ViewChild('solutionEditor') solutionEditor: AceEditorComponent;
   constructor(private fb: FormBuilder,
               private assignmentsService: AssignmentsService,
-              private router: Router) {
+              private router: Router,
+              private route: ActivatedRoute,
+              public location: Location,
+              private dataService: DataService
+              ) {
   }
 
   ngOnInit(): void {
@@ -36,6 +43,7 @@ export class CreateAssignmentComponent implements OnInit {
         this.assignmentForm.controls['duration'].enable();
       } else {
         this.assignmentForm.controls['duration'].disable();
+        this.assignmentForm.controls['duration'].reset();
       }
     });
     this.assignmentForm.controls['hasStartTime'].valueChanges.subscribe(v => {
@@ -43,6 +51,7 @@ export class CreateAssignmentComponent implements OnInit {
         this.assignmentForm.controls['startTime'].enable();
       } else {
         this.assignmentForm.controls['startTime'].disable();
+        this.assignmentForm.controls['startTime'].reset();
       }
     });
     this.assignmentForm.controls['hasDeadline'].valueChanges.subscribe(v => {
@@ -50,18 +59,38 @@ export class CreateAssignmentComponent implements OnInit {
         this.assignmentForm.controls['deadline'].enable();
       } else {
         this.assignmentForm.controls['deadline'].disable();
+        this.assignmentForm.controls['deadline'].reset();
       }
     });
+    const slug = this.route.snapshot.paramMap.get('slug');
+    if(slug){
+      this.isUpdateMode = true;
+      this.assignmentsService.getAssignmentBySlug(slug).subscribe(
+        (assignment)=>{
+          this.assignmentID = assignment.id;
+        this.assignmentForm.patchValue(assignment);
+      })
+    }
   }
 
   submit(): void {
-    const newAssignment: Assignment = this.assignmentForm.value;
-    this.assignmentsService.createAssignment(newAssignment).subscribe(
+    if(this.isUpdateMode){
+      const assignment: Assignment = this.assignmentForm.value;
+      this.assignmentsService.updateAssignment(this.assignmentID, assignment).subscribe(
+        (res: any) => {
+          this.router.navigate(['assignments', res.slug]);
+        }
+      )
+    }
+    else{
+      const newAssignment: Assignment = this.assignmentForm.value;
+      this.assignmentsService.createAssignment(newAssignment).subscribe(
       res => {
         console.log('Assignment created');
         this.router.navigate(['assignments', res.slug]);
     },
     console.error
     );
+    }
   }
 }
