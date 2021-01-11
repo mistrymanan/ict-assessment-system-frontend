ba3f981855b0342c2d69ab60759dd0dab337532e
mistrymanan <manan.cba1718@ict.gnu.ac.in>
2021-01-11 10:53:19 +0530

    app.module changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -55,6 +55,7 @@ import {environment} from '../environments/environment';
 import {AuthComponent} from './views/auth/auth.component';
 import {authInterceptorProviders} from './views/auth/token.interceptor';
 import {ModalModule} from 'ngx-bootstrap/modal';
+import { AddClassroomComponent } from './views/classroom/add-classroom/add-classroom.component';
 
 @NgModule({
   imports: [
@@ -87,7 +88,8 @@ import {ModalModule} from 'ngx-bootstrap/modal';
     P500Component,
     UserAssignmentViewComponent,
     ViewQuestionComponent,
-    AuthComponent
+    AuthComponent,
+    AddClassroomComponent
   ],
   providers: [{
     provide: LocationStrategy,
