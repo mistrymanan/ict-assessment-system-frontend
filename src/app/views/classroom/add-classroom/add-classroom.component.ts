a808da6b2cb529f7ee39ec2fb4aadeb7d48fa59c
mistrymanan <manan.cba1718@ict.gnu.ac.in>
2021-01-11 10:49:51 +0530

    add-classroom component added

---
+++ src/app/views/classroom/add-classroom/add-classroom.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-classroom',
+  templateUrl: './add-classroom.component.html',
+  styleUrls: ['./add-classroom.component.css']
+})
+export class AddClassroomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
