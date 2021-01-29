7694bfe111729bc84d1ba32d4bfdd607868ac967
mistrymanan <manan.cba1718@ict.gnu.ac.in>
2021-01-29 11:41:14 +0530

    user-service added

---
+++ src/app/models/user.ts
@@ -0,0 +1,7 @@
+export class User{
+    emailId:String
+    name:String
+    picture:String
+    instructClassrooms:Array<String>
+    enrolledClassrooms:Array<String>
+}
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {config} from '../config';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import { User } from '../models/user';
+import { NONE_TYPE } from '@angular/compiler';
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http:HttpClient) { }
+  createUser(email:String){
+    const data={emailId:email};
+    return this.http.post<User>(
+      `http://${config.host}/${config.endpoints.users}` ,
+      data,
+       {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+        }
+
+    )
+  });
+}
+
+patch(emailId:String){
+  return this.http.patch<User>(
+    `http://${config.host}/${config.endpoints.users}/${emailId}`,NONE_TYPE)
+}
+getRequest(emailID:String){
+  return this.http.get<User>(
+    `http://${config.host}/${config.endpoints.users}/${emailID}`
+  )
+}
+
+}
