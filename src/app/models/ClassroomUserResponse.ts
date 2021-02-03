e0c070255c6875713f40285c7a8c01366602c0a1
mistrymanan <manan.cba1718@ict.gnu.ac.in>
2021-02-03 11:58:57 +0530

    Classroom-User-Details-model added

---
+++ src/app/models/ClassroomUserResponse.ts
@@ -0,0 +1,6 @@
+import { ClassroomDetails } from "./classroom-details";
+
+export class ClassroomUserResponse{
+    instructClassrooms: ClassroomDetails[];
+    enrolledClassrooms: ClassroomDetails[];
+}
---
+++ src/app/models/classroom-details.ts
@@ -0,0 +1,7 @@
+export class ClassroomDetails{
+    id:String;
+    classroomSlug:String;
+    classroomTitle:String;
+    ownerEmail:String;
+    ownerName:String;
+}
