5f77ee9a927adaa61cc4c5477f8b1420ef634b24
Zahid Palsania <palsania1234@gmail.com>
2021-04-16 15:07:19 +0530

    Pagination added in user details

--- src/app/views/admin/admin-users/user-details/user-details.component.ts
+++ src/app/views/admin/admin-users/user-details/user-details.component.ts
@@ -1,26 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminUser } from '../../../../models/admin-user';
+import { PagerService } from '../../../../services/pager.service';
 import { UserService } from '../../../../services/user.service';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
   styleUrls: ['./user-details.component.css']
 })
 export class UserDetailsComponent implements OnInit {
-  adminuser: AdminUser[]
-  constructor(private adminuserservice:UserService) {
+ adminuser: AdminUser[]
+ private allItems: AdminUser[];
+
+  constructor(private adminuserservice:UserService,
+              private pagerService: PagerService) {
 
    }
+   // pager object
+   pager: any = {};
+
+   // paged items
+   pagedItems: any[];
 
   ngOnInit(): void {
     this.getAdminUser();
-  
+    // this.http.get('')
+    // .pipe(map((response: Response) => response.json()))
+    // .subscribe(data => {
+    //     this.allItems = data;
+    //     this.setPage(1);
+    // });
   }
+  setPage(page: number) {
+    // get pager object from service
+    this.pager = this.pagerService.getPager(this.allItems.length, page);
+
+    // get current page of items
+    this.pagedItems = this.allItems.slice(this.pager.startIndex, this.pager.endIndex + 1);
+}
 getAdminUser(){
   this.adminuserservice.getadminUser().subscribe(
     (response)=>{
       this.adminuser=response
+      this.allItems = response;
+      this.setPage(1);
     console.log(this.adminuser)
     }
   )
